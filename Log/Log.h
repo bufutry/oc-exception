c320ae5ae36446ab2093613891b5d6a1361b2619
Fucker <290358009@qq.com>
2016-03-15 10:36:37 +0800

    Initial Commit

---
+++ Log/Log.h
@@ -0,0 +1,13 @@
+//
+//  Log.h
+//  Log
+//
+//  Created by 杨绍鑫 on 16/3/15.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Log : NSObject
+
+@end
---
+++ Log/Log.m
@@ -0,0 +1,13 @@
+//
+//  Log.m
+//  Log
+//
+//  Created by 杨绍鑫 on 16/3/15.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import "Log.h"
+
+@implementation Log
+
+@end
